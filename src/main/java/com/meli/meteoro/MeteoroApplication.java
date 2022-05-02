7c830642675be7188798ce894c613e6b2aabdfc6
Diego Velez <diavelez@bancolombia.com.co>
2022-05-02 16:11:54 -0500

    project init

---
+++ src/main/java/com/meli/meteoro/MeteoroApplication.java
@@ -0,0 +1,13 @@
+package com.meli.meteoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/meteoro/MeteoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.meteoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
